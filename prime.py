9e13754e2a7fa098ef6c2bc869a0eb3a4d6157de
RituRai11 <c0963185@mylambton.ca>
2025-02-22 21:18:04 -0500

    Step 2: Handle input 1

--- prime.py
+++ prime.py
@@ -5,4 +5,6 @@ def generate_prime_factors(n):
     """Generate prime factors of a number"""
     if not isinstance(n, int):
         raise ValueError("Input must be an integer")
+    if n == 1:
+        return []
     return []
